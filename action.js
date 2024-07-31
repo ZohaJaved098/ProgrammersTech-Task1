e0a59da20d1d2f4564694fd9fd05584176db3cc0
ZohaJaved098 <138484112+ZohaJaved098@users.noreply.github.com>
2024-07-31 19:42:18 +0500

    Cleaning code

--- action.js
+++ action.js
@@ -15,22 +15,3 @@ const handleScroll = () => {
 window.addEventListener('scroll', handleScroll);
 handleScroll(); // Initial call to handle already visible items
 });
-
-
-
-// document.getElementById('contactForm').addEventListener('submit', function(event) {
-//     document.getElementById('confirmationMessage').style.display = 'block'; // Show the confirmation message
-//     setTimeout(function() {
-//         document.getElementById('confirmationMessage').style.display = 'none'; // Hide the message after 5 seconds
-//     }, 5000);
-// });
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Using Font Awesome icon for demonstration
-//     var iconHTML = '<i class="fa fa-android"></i>';
-//     var pageTitle = "Zoha's Portfolio";
-    
-//     // Set the document title with the icon
-//     document.title = iconHTML + ' ' + pageTitle;
-// });
